9d4836335ce7d0cfa500b84d82520d239d532588
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-05-05 03:12:45 +0600

    Cleaning up CQRS backport for v1.

--- Framework/Source/Lokad.Cqrs.Shared/Extensions/ExtendICollection.cs
+++ Framework/Source/Lokad.Cqrs.Shared/Extensions/ExtendICollection.cs
@@ -33,40 +33,5 @@ namespace Lokad
             }
             return collection;
         }
-
-        /// <summary>
-        /// Removes all items from the target collection
-        /// </summary>
-        /// <typeparam name="T">type of the item within the collection</typeparam>
-        /// <param name="collection">The collection.</param>
-        /// <param name="items">The items.</param>
-        /// <returns>same collection instance</returns>
-        public static ICollection<T> RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> items)
-        {
-            if (collection == null) throw new ArgumentNullException("collection");
-            if (items == null) throw new ArgumentNullException("items");
-
-            foreach (var item in items)
-            {
-                collection.Remove(item);
-            }
-
-            return collection;
-        }
-
-        /// <summary>
-        /// Shortcut to determine whether the specified <see cref="ICollection{T}"/> is empty.
-        /// </summary>
-        /// <typeparam name="T">items in the collection</typeparam>
-        /// <param name="self">The collection.</param>
-        /// <returns>
-        /// 	<c>true</c> if the specified self is empty; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsEmpty<T>(this ICollection<T> self)
-        {
-            if (self == null) throw new ArgumentNullException("self");
-
-            return self.Count == 0;
-        }
     }
 }
