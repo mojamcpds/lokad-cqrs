849ab56bbd6c3247b7103d5f1bdf9cb1c9323366
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-05-04 20:18:34 +0600

    Dropped rules from the backport.

--- Framework/Source/Lokad.Cqrs.Shared/Reflection/Express.cs
+++ Framework/Source/Lokad.Cqrs.Shared/Reflection/Express.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Linq.Expressions;
 using System.Reflection;
-using Lokad.Rules;
 
 namespace Lokad.Reflection
 {
@@ -17,15 +16,6 @@ namespace Lokad.Reflection
     /// </summary>
     public static class Express
     {
-        static Rule<LambdaExpression> LambdaIs(ExpressionType type)
-        {
-            return (expression, scope) =>
-                {
-                    if (expression.Body.NodeType != type)
-                        scope.Error("Lambda expression must be of type {0}", type);
-                };
-        }
-
         /// <summary>
         /// Gets the <see cref="MethodInfo"/> 
         /// from the provided <paramref name="method"/>.
@@ -34,95 +24,11 @@ namespace Lokad.Reflection
         /// <returns>method information</returns>
         public static MethodInfo MethodWithLambda(LambdaExpression method)
         {
-            Enforce.Argument(() => method, LambdaIs(ExpressionType.Call));
+            if (method == null) throw new ArgumentNullException("method");
+            if (method.Body.NodeType != ExpressionType.Call)
+                throw new ArgumentException("Expression should be of call type");
 
             return ((MethodCallExpression) method.Body).Method;
-        }
-
-        /// <summary> Gets the <see cref="ConstructorInfo"/> from the 
-        /// provided <paramref name="constructor"/> lambda. </summary>
-        /// <param name="constructor">The constructor expression.</param>
-        /// <returns>constructor information</returns>
-        public static ConstructorInfo ConstructorWithLamda(LambdaExpression constructor)
-        {
-            Enforce.Argument(() => constructor, LambdaIs(ExpressionType.New));
-
-            return ((NewExpression) constructor.Body).Constructor;
-        }
-
-        /// <summary> Gets the <see cref="MemberInfo"/> (field or property) 
-        /// from the  provided <paramref name="member"/> </summary>
-        /// <param name="member">The property expression.</param>
-        /// <returns>member information</returns>
-        public static MemberInfo MemberWithLambda(LambdaExpression member)
-        {
-            Enforce.Argument(() => member, LambdaIs(ExpressionType.MemberAccess));
-            return ((MemberExpression) member.Body).Member;
-        }
-
-        /// <summary> Gets the <see cref="PropertyInfo"/> from the provided
-        /// <paramref name="property"/> expression. </summary>
-        /// <param name="property">The property expression.</param>
-        /// <returns>property information</returns>
-        public static PropertyInfo PropertyWithLambda(LambdaExpression property)
-        {
-            var value = MemberWithLambda(property);
-            if (value.MemberType != MemberTypes.Property)
-                throw Errors.InvalidOperation("Member must be a property reference");
-
-            return (PropertyInfo) value;
-        }
-
-        /// <summary> Gets the <see cref="FieldInfo"/> from the provided 
-        /// <paramref name="field"/> expression. </summary>
-        /// <param name="field">The field expression.</param>
-        /// <returns>field information</returns>
-        public static FieldInfo FieldWithLambda(LambdaExpression field)
-        {
-            var value = MemberWithLambda(field);
-            if (value.MemberType != MemberTypes.Field)
-                throw Errors.InvalidOperation("Member must be a field reference");
-
-            return (FieldInfo) value;
-        }
-
-        /// <summary> Gets the <see cref="MethodInfo"/> 
-        /// from the provided <paramref name="method"/>.
-        /// </summary>
-        /// <param name="method">The method expression.</param>
-        /// <returns>method information</returns>
-        public static MethodInfo Method(Expression<Action> method)
-        {
-            return MethodWithLambda(method);
-        }
-
-        /// <summary>
-        /// Gets the <see cref="ConstructorInfo"/> 
-        /// from the provided <paramref name="constructorExpression"/>.
-        /// </summary>
-        /// <param name="constructorExpression">The constructor expression.</param>
-        /// <returns>constructor information</returns>
-        public static ConstructorInfo Constructor<T>(Expression<Func<T>> constructorExpression)
-        {
-            return ConstructorWithLamda(constructorExpression);
-        }
-
-        /// <summary> Gets the <see cref="PropertyInfo"/> from the provided
-        /// <paramref name="property"/> expression. </summary>
-        /// <param name="property">The property expression.</param>
-        /// <returns>property information</returns>
-        public static PropertyInfo Property<T>(Expression<Func<T>> property)
-        {
-            return PropertyWithLambda(property);
-        }
-
-        /// <summary> Gets the <see cref="FieldInfo"/> from the provided 
-        /// <paramref name="field"/> expression. </summary>
-        /// <param name="field">The field expression.</param>
-        /// <returns>field information</returns>
-        public static FieldInfo Field<T>(Expression<Func<T>> field)
-        {
-            return FieldWithLambda(field);
         }
     }
 
@@ -139,28 +45,6 @@ namespace Lokad.Reflection
         public static MethodInfo Method(Expression<Action<TTarget>> method)
         {
             return Express.MethodWithLambda(method);
-        }
-
-        /// <summary> Gets the <see cref="PropertyInfo"/> from the provided
-        /// <paramref name="property"/> expression. </summary>
-        /// <param name="property">The property expression.</param>
-        /// <returns>property information</returns>
-        /// <seealso cref="Express.MemberWithLambda"/>
-        /// <seealso cref="Express.PropertyWithLambda"/>
-        public static PropertyInfo Property<T>(Expression<Func<TTarget, T>> property)
-        {
-            return Express.PropertyWithLambda(property);
-        }
-
-        /// <summary> Gets the <see cref="FieldInfo"/> from the provided 
-        /// <paramref name="field"/> expression. </summary>
-        /// <param name="field">The field expression.</param>
-        /// <returns>field information</returns>
-        /// <seealso cref="Express.MemberWithLambda"/>
-        /// <seealso cref="Express.FieldWithLambda"/>
-        public static FieldInfo Field<T>(Expression<Func<TTarget, T>> field)
-        {
-            return Express.FieldWithLambda(field);
         }
     }
 }
