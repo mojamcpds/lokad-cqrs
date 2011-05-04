fd11160a82ffced27e2e93ad76ef454bcdc4ef27
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-05-04 17:48:29 +0600

    Decoupled NHibernate module from the Lokad.Cqrs.

--- Framework/Features/Lokad.Cqrs.NHibernate/NHibernateStarter.cs
+++ Framework/Features/Lokad.Cqrs.NHibernate/NHibernateStarter.cs
@@ -1,43 +1,43 @@
-﻿#region (c) 2010 Lokad Open Source - New BSD License 
+﻿//#region (c) 2010 Lokad Open Source - New BSD License 
 
-// Copyright (c) Lokad 2010, http://www.lokad.com
-// This code is released as Open Source under the terms of the New BSD Licence
+//// Copyright (c) Lokad 2010, http://www.lokad.com
+//// This code is released as Open Source under the terms of the New BSD Licence
 
-#endregion
+//#endregion
 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using NHibernate;
+//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using NHibernate;
 
-namespace Lokad.Cqrs.NHibernate
-{
-	[UsedImplicitly]
-	public sealed class NHibernateStarter : IEngineProcess
-	{
-		// is needed to initialize NHibernate on start.
-		readonly ISessionFactory _factory;
-		readonly ILog _log;
+//namespace Lokad.Cqrs.NHibernate
+//{
+//    [UsedImplicitly]
+//    public sealed class NHibernateStarter : IEngineProcess
+//    {
+//        // is needed to initialize NHibernate on start.
+//        readonly ISessionFactory _factory;
+//        readonly ILog _log;
 
-		public NHibernateStarter(ISessionFactory factory, ILogProvider provider)
-		{
-			_factory = factory;
-			_log = provider.LogForName<NHibernateStarter>();
-		}
+//        public NHibernateStarter(ISessionFactory factory, ILogProvider provider)
+//        {
+//            _factory = factory;
+//            _log = provider.LogForName<NHibernateStarter>();
+//        }
 
 
-		public void Dispose()
-		{
-		}
+//        public void Dispose()
+//        {
+//        }
 
-		public void Initialize()
-		{
-			_log.DebugFormat("NHibernate started: {0}", _factory.Statistics.StartTime);
-		}
+//        public void Initialize()
+//        {
+//            _log.DebugFormat("NHibernate started: {0}", _factory.Statistics.StartTime);
+//        }
 
-		public Task Start(CancellationToken token)
-		{
-			return Task.Factory.StartNew(() => { }, token);
-		}
-	}
-}
+//        public Task Start(CancellationToken token)
+//        {
+//            return Task.Factory.StartNew(() => { }, token);
+//        }
+//    }
+//}
